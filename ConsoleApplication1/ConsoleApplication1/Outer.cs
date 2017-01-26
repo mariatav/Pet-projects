92d64c5451262bcb0e04fe2b8dddd355bd71d888
Maria Tavlaki <mariataylaki@gmail.com>
2017-01-26 19:42:09 +0200

    Test nested class accessibility modifiers

---
+++ ConsoleApplication1/ConsoleApplication1/Outer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Outer
+    {
+        public int publicOuterVariable;
+        private int privateOuterVariable;
+
+        private void Method1()
+        {
+            int temp1=new Inner().publicInnerVariable;
+            int temp2 = new Inner().privateInnerVariable;
+        }
+
+        class Inner
+        {
+            public int publicInnerVariable;
+            private int privateInnerVariable;
+
+            private void Method2()
+            {
+                int temp1 = new Outer().publicOuterVariable;
+                int temp2 = new Outer().privateOuterVariable;
+            }
+        }
+    }
+}
