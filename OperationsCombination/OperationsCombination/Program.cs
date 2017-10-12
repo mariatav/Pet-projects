b8c6c1de69ac2cea4dd8d12ce515f4406f8ea64b
Maria Tavlaki <mariataylaki@hotmail.com>
2017-10-12 03:59:55 +0300

    Commit2

--- OperationsCombination/OperationsCombination/Program.cs
+++ OperationsCombination/OperationsCombination/Program.cs
@@ -24,66 +24,119 @@ namespace OperationsCombination
         {
             if (operationSequence != null)
                 return operationSequence;
-            var operationStack = new LinkedList<Operation>();
+
+            var operationStack = new LinkedList<List<Operation>>();
             foreach (var num in this._numbers)
-                operationStack.AddFirst(new Operation())
+                operationStack.AddFirst(new List<Operation> { new Operation(num) });
+
+            while (operationStack.Count != 0)
+            {
+                var currentOperations = operationStack.First.Value;
+                operationStack.RemoveFirst();
+
+                for (int i = 0; i < currentOperations.Count; i++)
+                {
+                    var operation = currentOperations[i];
+                    if (operation.Result == this._target)
+                        return "found it!!";
+                    for (int j = i + 1; j < currentOperations.Count; j++)
+                    {
+                        var newOperations = new List<Operation>();
+                        if (Operation.TryApplyOperator(currentOperations[i], currentOperations[j], Operator.Add,
+                            out var newOperation))
+                            newOperations.Add(newOperation);
+                        if (Operation.TryApplyOperator(currentOperations[i], currentOperations[j], Operator.Multiply,
+                            out newOperation))
+                            newOperations.Add(newOperation);
+                        if (Operation.TryApplyOperator(currentOperations[i], currentOperations[j], Operator.DivideBy,
+                            out newOperation))
+                            newOperations.Add(newOperation);
+                        if (Operation.TryApplyOperator(currentOperations[i], currentOperations[j], Operator.Subtract,
+                            out newOperation))
+                            newOperations.Add(newOperation);
+                        if (Operation.TryApplyOperator(currentOperations[j], currentOperations[i], Operator.DivideBy,
+                            out newOperation))
+                            newOperations.Add(newOperation);
+                        if (Operation.TryApplyOperator(currentOperations[j], currentOperations[i], Operator.Subtract,
+                            out newOperation))
+                            newOperations.Add(newOperation);
+                    }
+                }
+            }
             return null;
         }
 
 
-        private enum Operator
+        public enum Operator
         {
             Multiply,
             Add,
             Subtract,
             DivideBy
         }
-        private class Operation
+
+        public class Operation
         {
-            public int LeftOperand { get; private set; }
-            public int RightOperand { get; private set; }
+            public Operation LeftOperand { get; private set; }
+            public Operation RightOperand { get; private set; }
             public Operator Operator { get; private set; }
             public int Result { get; private set; }
+
+            public Operation(int number)
+            {
+                this.Result = number;
+            }
+
+            private Operation()
+            {
+            }
 
             public bool Equals(Operation other)
             {
                 return this.Result == ((Operation)other).Result;
             }
 
-            public static bool TryApplyOperator(int _rightOperand, int _leftOperand,
-                Operator _operator, out Operation _operation)
+            public static bool TryApplyOperator(Operation _rightOperand, Operation _leftOperand,
+                Operator _operator, out Operation _result)
             {
                 double result = 0;
                 switch (_operator)
                 {
                     case Operator.Add:
-                        result = _rightOperand + _leftOperand;
+                        result = _rightOperand.Result + _leftOperand.Result;
                         break;
                     case Operator.Multiply:
-                        result = _rightOperand * _leftOperand;
+                        result = _rightOperand.Result * _leftOperand.Result;
                         break;
                     case Operator.DivideBy:
-                        result = (double)_rightOperand / _leftOperand;
+                        result = (double)_rightOperand.Result / _leftOperand.Result;
                         break;
                     case Operator.Subtract:
-                        result = _rightOperand - _leftOperand;
+                        result = _rightOperand.Result - _leftOperand.Result;
                         break;
                 }
                 if (result < 0 || result != (int)result)
                 {
-                    _operation = null;
+                    _result = null;
                     return false;
                 }
-                _operation = new Operation() { LeftOperand = _leftOperand, RightOperand = _rightOperand, Operator = _operator, Result = (int)result };
+                _result = new Operation()
+                {
+                    LeftOperand = _leftOperand,
+                    RightOperand = _rightOperand,
+                    Operator = _operator,
+                    Result = (int)result
+                };
                 return true;
             }
         }
 
-    }
-    class Program
-    {
+
+
         static void Main(string[] args)
         {
         }
+
     }
 }
+
